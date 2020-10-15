9a2e6618aff2c09bb2c7ac14c4937297ff0fe592
deep846 <45318452+deep846@users.noreply.github.com>
2020-10-15 14:00:35 +0530

    solved the issue

--- while loop.py
+++ while loop.py
@@ -2,6 +2,8 @@ print("Hello everyone, we are starting")
 n=1
 c=1
 while (c==1):
-    print
+    print("token number",n,"please come in")
+    c=input("Continue?(0/1)")
+    c=int(c) 
     n=n+1
-print("Thank you, this nd of our tip for 
+print("Thank you, this is the end of our tip for today")
